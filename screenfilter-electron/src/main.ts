1e3b127e55963237c25aaf1725cd6ec8ba879da8
Matthew Power <m13power@gmail.com>
2026-02-20 12:29:00 -0700

    feat: configure window properties

--- screenfilter-electron/src/main.ts
+++ screenfilter-electron/src/main.ts
@@ -1,5 +1,15 @@
-const { BrowserWindow } = require('electron');
+import { BrowserWindow } from 'electron';
 
-let browserWindow = new BrowserWindow();
+// WARN: The focusable flag that allows input passthrough only works on windows
+// and MacOS.
+let browserWindow = new BrowserWindow({
+    frame: false,
+    focusable: false, // Windows, MacOS
+    fullscreen: true,
+    transparent: true,
+    // This apparently exists but my LSP complains
+    // visibleOnAllWorkspaces: true, // MacOS, Linux
+    alwaysOnTop: true,
+});
+
 browserWindow.setIgnoreMouseEvents(true);
-
